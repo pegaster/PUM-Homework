1efbd4fb17038f6a7c7bf955835befc60c062415
pegaster <steve.dneprov@gmail.com>
2020-11-10 10:31:19 +0300

    Исправил третье задание

--- ON_Lesson/3/main.cpp
+++ ON_Lesson/3/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -8,7 +9,17 @@ double D(float a, float b, float c){
 
 int main(){
     float a, b, c;
+    double Discriminant;
     cin >> a >> b >> c;
-    cout << D(a, b, c) << endl;
+    Discriminant = D(a, b, c);
+    if(Discriminant >= 0){
+        cout << (-b + sqrt(Discriminant)) / (a * 2) << endl;
+    }
+    if(Discriminant > 0){
+        cout << (-b - sqrt(Discriminant)) / (a * 2) << endl;
+    }
+    if(Discriminant < 0){
+        cout << -b / (a * 2) << " +- i * " << sqrt(-Discriminant) / (2 * a) << endl;
+    }
     return 0;
 }
